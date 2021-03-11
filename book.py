d01a96f7610877f866a785614a01e8a6ea894e9f
Ishaan Kale <ishaannk@outlook.com>
2021-03-11 18:41:58 +0530

    i

---
+++ book.py
@@ -0,0 +1,5 @@
+def favourite_book(title):
+    '''Display a person's favourite book.'''
+    print(f'One of my favourite books is {title.title()}.')
+
+favourite_book('harry potter')
---
+++ shirt.py
@@ -0,0 +1,7 @@
+def make_shirt(size, text):
+    '''Makes a shirt with a size
+    and some text printed on it.'''
+    print(f'\nThe shirt\'s size is {size}')
+    print(f'\nand the text on the shirt is "{text}".')
+##
+make_shirt(28, 'Ishaan')
