5dec5728366c488c9f04a5047952848ca25f8e54
Ishaan Kale <ishaannk@outlook.com>
2021-03-11 17:53:39 +0530

    Added a parm and fixed a bug

--- person.py
+++ person.py
@@ -1,4 +1,9 @@
-def build_person(first_name, last_name):
+def build_person(first_name, last_name, age=None):
     """Return a dictionary of information about a person."""
     person = {'first': first_name, 'last': last_name}
+    if age:
+        person['age'] = age
     return person
+
+musician = build_person('ishaan', 'kale', age=9)
+print(musician)
