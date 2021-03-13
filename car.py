0245075cd15bb15c837eed4f240140b556ec515a
Ishaan Kale <ishaannk@outlook.com>
2021-03-13 09:31:43 +0530

    Added 3 docstrings

--- car.py
+++ car.py
@@ -1,9 +1,3 @@
-prompt = f'Give the name of the rental car you would would like to rent'
-prompt += '\nand I\'ll see if I can find you that car. '
-cars = ['duster', 'toyota', 'minia', 'volkswagon', 'honda']
-car = input(prompt).lower()
-
-if car in cars:
-    print(f'Here is your {car.title()}!')
-else:
-    print(f'Nope! I cannot give you the car {car.title()}.')
+class Car:
+    """A simple attempt to represent a car."""
+    
--- restarurant.py
+++ restarurant.py
@@ -1,11 +1,13 @@
 class Restarurant:
     """A simple attempt to model a rest.."""
 
-    def __init__(self, restarurunt_name, veggi):
-        self.name = restarurunt_name
+    def __init__(self, restarurant_name, veggi):
+        """Initialize restarurant_name & veggi attributts."""
+        self.name = restarurant_name
         self.is_veg = veggi
 
     def describe_restarurant(self):
+        """Describe the restarurant."""
         print(f"This is {self.name.title()}!")
         if self.is_veg:
             print(f"{self.name.title()} is veg!")
@@ -13,6 +15,7 @@ class Restarurant:
             print(f"{self.name.title()} is non-veg!")
 
     def open_restarurant(self):
+        """Print a message that the restarurant has opened."""
         print(f"{self.name.title()} has opened!")
 
 my_restarurant = Restarurant('The BOOM restarurant', False)
