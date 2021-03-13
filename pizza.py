7841ab5f5d8c5d405003a3c2e4c0031e22439310
Ishaan Kale <ishaannk@outlook.com>
2021-03-13 07:46:58 +0530

    Updated a function

--- pizza.py
+++ pizza.py
@@ -1,6 +1,8 @@
-def make_pizza(*toppings):
-    """Print the list of toppings that have been requested."""
-    print(toppings)
+def make_pizza(size, *toppings):
+    """Summarize the pizza we are going to make."""
+    print(f"\nMaking a {size}-inch pizza with the following toppings:")
+    for topping in toppings:
+        print(f"- {topping}")
 
-make_pizza('pepperoni')
-make_pizza('mushrooms', 'green peprers', 'extra cheese')
+make_pizza(5525252, 'pepperoni')
+make_pizza(9999999, 'mushrooms', 'green peprers', 'extra cheese')
