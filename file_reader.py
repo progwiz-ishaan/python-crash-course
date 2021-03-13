46ff2f7a771ed55837166e49c624e6e0a3cbb6ed
Ishaan Kale <ishaannk@outlook.com>
2021-03-13 15:57:28 +0530

    Added a millon digits

--- file_reader.py
+++ file_reader.py
@@ -1,3 +1,11 @@
-with open('pi_digits.txt') as file_object:
-    contents = file_object.read()
-    print(contents.rstrip())
+filename = 'pi_digits.txt'
+
+with open(filename) as file_object:
+    lines = file_object.readlines()
+
+pi_string = ''
+for line in lines:
+    pi_string += line.strip()
+
+print(pi_string)
+print(len(pi_string))
