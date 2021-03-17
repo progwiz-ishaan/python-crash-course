7c1fbf6663e09f0bf23825e8075ff83a41ea9876
Ishaan Kale <ishaannk@outlook.com>
2021-03-17 16:50:25 +0530

    q

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -15,13 +15,16 @@ class Ship:
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
-        # Movement flag
+        # Movement flags
+        self.moving_left = False
         self.moving_right = False
 
     def update(self):
-        """Update the ship's position based on the flag."""
+        """Update the ship's position based on the flags."""
         if self.moving_right:
             self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
      
     def blitme(self):
         """Draw the ship at its current location."""
