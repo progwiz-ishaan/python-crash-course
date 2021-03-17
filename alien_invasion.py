84807405f8389fe5957e9accf9acd3565c9de5ff
Ishaan Kale <ishaannk@outlook.com>
2021-03-17 11:20:52 +0530

    Added a class and a if statment.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,1 +1,29 @@
+import sys
+
 import pygame
+
+class AlienInvasion:
+    """Overall class to manage game assets a behiveiour."""
+
+    def __init__(self):
+        """Initialize thegame, and create the game resourses."""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    # Make a game istance and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
