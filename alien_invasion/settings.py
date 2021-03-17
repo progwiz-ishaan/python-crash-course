eb0314971cdc769d33a1487156df7cda7ab5299f
Ishaan Kale <ishaannk@outlook.com>
2021-03-17 12:03:04 +0530

    Created a class

---
+++ alien_invasion/settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """A class to store all the settings for alien_invasion.py."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_colour = (230, 230, 230)
