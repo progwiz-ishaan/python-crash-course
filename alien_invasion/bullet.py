c1341bc3b1544180fabea4fa12c396510c2e4e39
Ishaan Kale <ishaannk@outlook.com>
2021-03-18 10:06:56 +0530

    Added a docstring

--- alien_invasion/bullet.py
+++ alien_invasion/bullet.py
@@ -5,6 +5,7 @@ class Bullet(Sprite):
     """A class to manage bullets fired form the ship."""
 
     def __init__(self, ai_game):
+        """Initialize the bullets."""
         super().__init__()
         self.screen = ai_game.screen
         self.settings = ai_game.settings
