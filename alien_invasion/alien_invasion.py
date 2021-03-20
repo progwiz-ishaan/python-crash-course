ca1619afc6587840df881d63fdb8e9b9c9c4ee11
Ishaan Kale <ishaannk@outlook.com>
2021-03-20 14:36:18 +0530

    4

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -91,6 +91,7 @@ class AlienInvasion:
                 # Reset the game statistics.
                 self.stats.reset_stats()
                 self.stats.game_active = True
+                self.sb.prep_score()
 
                 # Get rid of any remaining aliens or bullets
                 self.aliens.empty()
