86125c954f74cfdbc4c9e52c4c7df22b561ad56d
Ishaan Kale <ishaannk@outlook.com>
2021-03-20 13:41:44 +0530

    Displayed the score.

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -9,6 +9,7 @@ from bullet import Bullet
 from alien import Alien
 from game_stats import GameStats
 from button import Button
+from scoreboard import Scoreboard
 
 class AlienInvasion:
     """Overall class to manage game assets a behiveiour."""
@@ -23,8 +24,10 @@ class AlienInvasion:
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
 
-        # Create an instance to store game statistics
+        # Create an instance to store game statistics,
+        #   and create a score board
         self.stats = GameStats(self)
+        self.sb = Scoreboard(self)
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
@@ -143,6 +146,9 @@ class AlienInvasion:
         # Draw the play button if the game is inactive.
         if not self.stats.game_active:
             self.play_button.draw_button()
+
+        # Draw the score to show information.
+        self.sb.show_score()
 
         pygame.display.flip()
 
