f09717f369d7cc0b6fcfbda166f1d942996f8b23
Ishaan Kale <ishaannk@outlook.com>
2021-03-21 13:32:31 +0530

    4

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -92,6 +92,7 @@ class AlienInvasion:
                 self.stats.reset_stats()
                 self.stats.game_active = True
                 self.sb.prep_score()
+                self.sb.prep_level()
 
                 # Get rid of any remaining aliens or bullets
                 self.aliens.empty()
@@ -134,12 +135,17 @@ class AlienInvasion:
             for aliens in collisions.values():
                 self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
+            self.sb.check_high_score()
 
         if not self.aliens:
             # Destory the exsisting bullets and create a new fleet
             self.bullets.empty()
             self._create_fleet()
             self.settings.increse_speed_settings()
+
+            # Increse level
+            self.stats.level += 1
+            self.sb.prep_level()
 
     def _update_screen(self):
         """Update the screen."""
