f372c64621d512dc5ccdf9ff6e620a4346053312
Ishaan Kale <ishaannk@outlook.com>
2021-03-31 13:30:43 +0530

    i

--- sitka_highs.py
+++ sitka_highs.py
@@ -1,7 +1,27 @@
 import csv
 
-filename = 'C:/Users/ishaa/Documents/Python-Scripts/python-crash-course/data/sitka_weather_07-2014.csv'
+import matplotlib.pyplot as plt
+
+filename = '/home/ishaan/Documents/Ishaan\'s-Coding/python-crash-course/data/sitka_weather_07-2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    print(header_row)
+
+    # Get high tempretures from this file.
+    highs = []
+    for row in reader:
+        high = int(row[5])
+        highs.append(high)
+
+# Plot the high tempretures.
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(highs, c='red')
+
+# Format plot.
+plt.title("Sitka weather daily high tempretures July 2014", fontsize=24)
+plt.xlabel('', fontsize=16)
+plt.ylabel("Tempreture (F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
