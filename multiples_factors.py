709ef16d514823d4103bd7be4a383aff8d4d12d7
Ishaan Kale <ishaannk@outlook.com>
2021-04-04 12:56:18 +0530

    7

--- multiples_factors.py
+++ multiples_factors.py
@@ -8,3 +8,12 @@ def get_factors(number):
             factors.append(number_to_divide)
 
     return factors
+
+def get_multiples(number, till):
+    """Returns all the multiples of a given number till a given number."""
+    multiples = []
+    for number1 in range(1, till+1):
+        multiple = number1 * number
+        multiples.append(multiple)
+
+    return multiples
--- test_.py
+++ test_.py
@@ -1,16 +1,21 @@
 import unittest
-from multiples_factors import get_factors
+import multiples_factors
 
 class _test_(unittest.TestCase):
     """A test class for the v2math module."""
 
     def setUp(self):
         """Setting up."""
-        self.num = get_factors(6)
+        self.num = multiples_factors.get_factors(6)
+        self.r = multiples_factors.get_multiples(6, 4)
 
     def test_get_factors(self):
         """Test get_factors()."""
         self.assertEqual(self.num, [1, 2, 3, 6])
+    
+    def test_get_multiples(self):
+        """Test get_multiples()."""
+        self.assertEqual(self.r, [6, 12, 18, 24])
 
 if __name__ == '__main__':
     unittest.main()
