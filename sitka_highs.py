81c821907f55d61fe22d6aaadcb47934128cb216
Ishaan Kale <ishaannk@outlook.com>
2021-04-11 16:04:29 +0530

    6

--- sitka_highs.py
+++ sitka_highs.py
@@ -1,25 +1,30 @@
 import csv
+from datetime import datetime
 
 import matplotlib.pyplot as plt 
 
-filename = '/home/ishaan/Documents/Ishaan\'s-Coding/python-crash-course/data/sitka_weather_07-2018_simple.csv'
+filename = '/home/ishaan/Documents/Ishaan\'s-Coding/python-crash-course/data/sitka_weather_2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    # Get high temperatures form this file.
-    highs = []
+    # Get dates and high temperatures form this file.
+    dates, highs = [], []
     for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
         high = int(row[5])
+        dates.append(current_date)
         highs.append(high)
 
 # Plot the high temptures.
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
+plt.plot(dates, highs, c='red')
 
 # Format plots
-plt.title("Daily high temperatures, July 2018", fontsize=24)
+plt.title("Daily high temperatures - 2018", fontsize=24)
 plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
 
