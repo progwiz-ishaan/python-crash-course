ca2cfe0375762b9f112a49441e4c5567e76261f2
Ishaan Kale <ishaannk@outlook.com>
2021-04-14 11:24:37 +0530

    8

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -6,4 +6,16 @@ with open(filename) as f:
     all_eq_data = json.load(f)
 
 all_eq_dicts = all_eq_data['features']
-print(len(all_eq_dicts))
+
+mags, lons, lats = [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    lons.append(lon)
+    lats.append(lat)
+    mags.append(mag)
+
+print(mags[:10])
+print(lons[:5])
+print(lats[:5])
---
+++ eq_world_map.py
@@ -0,0 +1,34 @@
+import json
+
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
+# Explore the stucure of the data.
+filename = '/home/ishaan/Documents/Ishaan\'s-Coding/python-crash-course/data/eq_data_1_day_m1.json'
+with open(filename) as f:
+    all_eq_data = json.load(f)
+
+all_eq_dicts = all_eq_data['features']
+
+mags, lons, lats = [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    lons.append(lon)
+    lats.append(lat)
+    mags.append(mag)
+
+# Map the earthquakes.
+data = [{
+    'type': 'scattergeo',
+    'lon': lons,
+    'lat': lats,
+    'marker': {
+        'size': [5*mag for mag in mags],
+    },
+}]
+my_layout = Layout(title="Global Earthquakes")
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='ge.html')
