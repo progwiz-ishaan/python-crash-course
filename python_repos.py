e913a225c9a93591fbaa1bf10fbb274a6565a657
Ishaan Kale <ishaannk@outlook.com>
2021-05-29 10:05:55 +0530

    3

--- python_repos.py
+++ python_repos.py
@@ -1,13 +1,35 @@
 import requests
 
-# Make an API call and store the response
+from plotly.graph_objs import Bar
+from plotly import offline
+
+# Make an API call and store the response.
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 headers = {'Accept': 'application/vnd.github.v3+json'}
 r = requests.get(url, headers=headers)
 print(f"Status code: {r.status_code}")
 
-# Store API response in a variable.
-reponse_dict = r.json()
+# Process results.
+response_dict = r.json()
+repo_dicts = response_dict['items']
+repo_names, stars = [], []
 
-# Prosses results.
-print(reponse_dict.keys())
+print("\nSelected information about each repositoriey:")
+for repo_dict in repo_dicts:
+    repo_names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+# Make visualazion.
+data = [{
+    'type': 'bar',
+    'x': repo_names,
+    'y': stars
+}]
+my_layout = {
+    'title': 'Most-Starred Python Projects on Github',
+    'xaxis': {'title': 'Repository'},
+    'yaxis': {'title': 'Stars'}
+}
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='python_repos.html')
