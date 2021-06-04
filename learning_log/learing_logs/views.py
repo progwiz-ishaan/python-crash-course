f4622d14cee0055abd593a5f92ef22789b59e836
Ishaan Kale <ishaannk@outlook.com>
2021-06-04 15:07:07 +0530

    s

--- learning_log/learing_logs/views.py
+++ learning_log/learing_logs/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    """The home page for Learning Log."""
+    return render(request, 'learing_logs/index.html')
