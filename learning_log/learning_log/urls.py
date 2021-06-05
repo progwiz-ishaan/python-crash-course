50b99ae482e3e284b816bc389f65e519ad2940bd
Ishaan Kale <ishaannk@outlook.com>
2021-06-05 13:01:16 +0530

    s

--- learning_log/learning_log/urls.py
+++ learning_log/learning_log/urls.py
@@ -1,4 +1,4 @@
-"""learing_log URL Configuration
+"""learning_log URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -18,5 +18,4 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('learning_log.urls'))
 ]
